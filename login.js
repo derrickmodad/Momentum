bbba3466089513910d86550275de3912d9e58d41
derrickmodad <modad_db@live.bluefieldstate.edu>
2025-04-29 22:50:28 -0400

    added toggleability between sign up and login forms

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
+"use strict";
+
 let loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const email = document.getElementById("emailInput").value;
-    const password = document.getElementById("passwordInput").value;
+    const email = document.getElementById("loginEmailInput").value;
+    const password = document.getElementById("loginPasswordInput").value;
 
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
@@ -11,9 +13,33 @@ loginForm.addEventListener("submit", async (e) => {
     });
 
     if (error) {
-      alert("Login failed: " + error.message);
+      console.log("Login failed: " + error.message);
     } else {
-      alert("Logged in!");
+      console.log("Logged in!");
       window.location.href = "index.html";
     }
 });
+
+function toggleForm(calling) {
+  let signUp = document.getElementById("signUpDiv");
+  let login = document.getElementById("loginDiv");
+  if (calling === 1) {
+    toggleFormFields(login, false);
+    toggleFormFields(signUp, true);
+    login.classList.add("hidden");
+    signUp.classList.remove("hidden");
+  } else {
+    toggleFormFields(signUp, false);
+    toggleFormFields(login, true);
+    signUp.classList.add("hidden");
+    login.classList.remove("hidden");
+  }
+
+}
+
+function toggleFormFields(container, enable) {
+  const fields = container.querySelectorAll("input, button");
+  fields.forEach(function(i) {
+      i.disabled = !enable;
+  });
+}
